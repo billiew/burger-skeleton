1ef68b49a49b008a764a45ee2bc4917b325ecdcb
BillieW <48058845+billiew@users.noreply.github.com>
2019-12-13 10:59:04 +0100

    counter not a button

--- src/components/Ingredient.vue
+++ src/components/Ingredient.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="ingredient">
     <label>
+      <span>
       <button id="orderbuttons" v-on:click="decrementCounter"> <img src="@/assets/negative-sign-button.png" height="15"></button>
-      <button id="orderbuttons" > {{counter}} </button>
+      {{counter}}
       <button id="orderbuttons"  v-on:click="incrementCounter"> <img src="@/assets/plusbutton.png" height="15"></button>
-
+      </span>
       <br>
       {{item["ingredient_"+ lang]}}
       <br>
