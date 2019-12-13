75feafe17e12b558478946ddd7a920c86fbd0114
BillieW <48058845+billiew@users.noreply.github.com>
2019-12-13 15:43:39 +0100

    uppdelat på kategori Kitchen

--- src/components/OrderItem.vue
+++ src/components/OrderItem.vue
@@ -1,11 +1,29 @@
 <template>
-	<div>
-		{{uiLabels.order}}: #{{orderId}}
-		<div>
-		{{order.type}} {{uiLabels.ingredients}}:
+	<div class="orderWrapper">
+ 		<span class="box ordernumb">{{uiLabels.order}}: #{{orderId}}</span>
+		<div class="box burger">
+		{{order.type}} {{uiLabels.hamburger}}:
+		<div v-for="item in order.ingredients":key="item.ingredient_id">
+			<span v-if="item.category<5">{{ item["ingredient_"+lang] }}</span>
 		</div>
-		<div v-for="item in order.ingredients" :key="item.ingredient_id">
-			{{ item["ingredient_"+lang] }}
+		</div>
+		<div class="box burger">
+		{{order.type}} {{uiLabels.hamburger}}:
+		<div v-for="item in order.ingredients":key="item.ingredient_id">
+			<span v-if="item.category<5">{{ item["ingredient_"+lang] }}</span>
+		</div>
+		</div>
+		<div class="box drink">
+		{{order.type}} {{uiLabels.drinks}}  :
+		<div v-for="item in order.ingredients":key="item.ingredient_id">
+			<span v-if="item.category===6">{{ item["ingredient_"+lang] }}</span>
+		</div>
+		</div>
+		<div class="box sides">
+		{{order.type}} {{uiLabels.sideorders}}  :
+		<div v-for="item in order.ingredients":key="item.ingredient_id">
+			<span v-if="item.category===5">{{ item["ingredient_"+lang] }}</span>
+		</div>
 		</div>
 		<button v-on:click="orderUndo">
 			{{uiLabels.notReady}}
@@ -31,5 +49,33 @@ export default {
 }
 </script>
 <style scoped>
+.orderWrapper{
+  display: grid;
+  grid-template-columns: 25% 25% 25% 25% ;
+}
 
+.box {
+		padding: 1em;
+		font-size: 90%;
+		text-align: center;
+
+}
+
+.ordernumb {
+		grid-column: 1;
+		grid-row: 1 / span 3;
+}
+.burger {
+		grid-column: 2;
+		grid-row: 1 / span 3;
+}
+.drink {
+		grid-column: 3 ;
+		grid-row: 1 / span 3;
+}
+
+.sides {
+		grid-column: 4 ;
+		grid-row: 1 / span 3;
+}
 </style>
