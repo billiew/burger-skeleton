cb17965a940054004efbf4efff5cb395112d9bf6
xtinealexa <christinearkbo@gmail.com>
2020-01-04 14:31:59 +0100

    Fraser och en ny component

---
+++ src/components/OrderItemToShow.vue
@@ -0,0 +1,49 @@
+<template>
+  <!-- Note in this component that it is using another component -->
+<div>
+  <OrderItem
+    :ui-labels="uiLabels"
+    :lang="lang"
+    :order-id="orderId"
+    :order="order">
+  </OrderItem>
+  <button v-on:click="orderNotStarted">
+    {{uiLabels.process}}
+  </button>
+</div>
+
+</template>
+<script>
+
+import OrderItem from '@/components/OrderItem.vue'
+
+export default {
+  name: 'OrderItemToPrepare',
+  components: { OrderItem },
+  props: {
+    uiLabels: Object,
+    order: Object,
+    orderId: String,
+    lang: String
+  },
+  methods: {
+    orderDone: function () {
+      // sending 'done' message to parent component or view so that it
+      // can catch it with v-on:done in the component declaration
+      this.$emit('process');
+    },
+    /*orderUndo: function () {
+			// sending 'done' message to parent component or view so that it
+			// can catch it with v-on:done in the component declaration
+			this.$emit('undo', this.orderId);
+			//this.$store.state.socket.emit('orderUndo', this.orderId);
+		},*/
+    cancelOrder: function () {
+      // not implemented
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
